92c37216763fd8520b1aeed01fbbae04007e811d
Juliovillarruel <juliovillarruel2000@gmail.com>
2025-04-14 23:43:02 -0700

    New

---
+++ 0.1[PrintFStatement].c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void)
+{
+
+    /* This section of the code triggers the basic fundamental command piece of code. 
+    This piece of the code is intednded to display how the software program simply triggers an 
+    event. This is what is called a printf statement. It is truly so easy to understand julio. 
+    */
+
+
+
+    printf("\n\nThis is the first robot rest\n\n");
+    printf("This is super duper easy to do julio trust me\n\n\n");
+
+
+    
+}
---
+++ 0.2[Formatting].c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main (void)
+{
+
+printf("\n\n\t\t\t\t\t-------------------------------------------");
+printf("\n           \b\t\t\t\t|\t\t\t\t\t   |");
+printf("\n           \b\t\t\t\t|\t\t\t\t\t   |");
+printf("\n           \bThis is an attempt at making a boxt\t\t|\t\t\t\t\t   |");
+printf("\n           \b\t\t\t\t|\t\t\t\t\t   |");
+printf("\n\t\t\t\t\t-------------------------------------------\n\n");
+
+
+}
