4e17f8f97605fbe6ef3e347bb5ee3cccff7a0dd1
Juliovillarruel <juliovillarruel2000@gmail.com>
2025-04-15 21:29:12 -0700

    I don't know I just made an update

--- 0.2_formatting.c
+++ 0.2_formatting.c
@@ -8,7 +8,7 @@ int main (void)
 printf("\n\n\t\t\t\t\t-------------------------------------------");
 printf("\n           \b\t\t\t\t|\t\t\t\t\t   |");
 printf("\n           \b\t\t\t\t|\t\t\t\t\t   |");
-printf("\n           \bThis is an attempt at making a boxt\t\t|\t\t\t\t\t   |");
+printf("\n           \b\t\t\t\t|\tThis is an attempt at making a box |");
 printf("\n           \b\t\t\t\t|\t\t\t\t\t   |");
 printf("\n\t\t\t\t\t-------------------------------------------\n\n");
 
