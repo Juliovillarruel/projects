a8608c032cf8cb5ecc8d1df81bafc3c6f6ea88bb
Juliovillarruel <juliovillarruel2000@gmail.com>
2025-05-15 05:54:21 -0700

    Beginner's Code

---
+++ 1.0.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    printf("|----------------------------------------------------------------------------------|\n");
+    printf("|\t\t\t\t\t\t\t\t\t\t   |\n");
+
+    printf("|\t\t\t\t    JULIO'S BANK\t\t\t\t   |\n");
+    printf("------------------------------------------------------------------------------------\n");
+    printf("|\t\t|\t\t|\t\t|\t\t|\t   \t   |\n");
+    printf("|\t\t|\t\t|\t\t|\t\t|\t   \t   |\n");
+    printf("|\t\t|\t\t|\t\t|\t\t|\t   \t   |\n");
+    printf("|\t\t|\t\t|\t\t|\t\t|\t   \t   |\n");
+    printf("|\t\t|\t\t|\t\t|\t\t|\t   \t   |\n");
+    printf("|\t\t|\t\t|\t\t|\t\t|\t   \t   |\n");
+    printf("|\t\t|\t\t|\t\t|\t\t|\t   \t   |\n");
+    printf("|\t\t|\t\t|\t\t|\t\t|\t   \t   |\n");
+
+
+    printf("|\t\t\b_/\t\t\b_/\t\t\\_\t\t\\_\t   \t   |\n");
+
+    printf("|\t\t\b\b\b_/\t\t/\t\t\b\\\t\t\\\t   \t   |\n");
+    
+    printf("|\t\t\b\b\b\b\b_/\t\t/\t\t\\\t\t\\\t   \t   |\n");
+    printf("|\t\t\b\b\b\b\b\b\b_/\t\t/\t\t\\\t\t\\\t   \t   |\n");
+    printf("|\t\t\b\b\b\b\b\b\b\b\b_/\t\t/\t\t\\\t\t\\\t   \t   |\n");
+    printf("|\t\t\b\b\b\b\b\b\b\b\b\b\b_/\t\t/\t\t\\\t\t\\\t   \t   |\n");
+    printf("|\t\t\b\b\b\b\b\b\b\b\b\b\b\b\b_/\t\t/\t\t\\\t\t\\\t   \t   |\n");
+    printf("|\t\t\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b_/\t\t/\t\t\\\t\t\\\t   \t   |\n");
+
+
+
+
+
+    printf("|\t\t\t\t\t\t\t\t\t\t   |\n");
+    printf("|\t\t\t\t\t\t\t\t\t\t   |\n");
+    printf("|\t\t\t\t\t\t\t\t\t\t   |\n");
+    printf("|\t\t\t\t\t\t\t\t\t\t   |\n");
+    printf("|\t\t\t\t\t\t\t\t\t\t   |\n");
+    printf("|\t\t\t\t\t\t\t\t\t\t   |\n");
+
+    printf("|----------------------------------------------------------------------------------|");
+
+
+
+
+}
