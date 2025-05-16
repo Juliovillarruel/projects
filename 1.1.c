7dae047055ee5135e95fc685f1ab119d85f690f3
Julio Villarruel <julio@Julios-MacBook-Pro.local>
2025-05-16 08:40:21 -0700

    This is a new and improved code

---
+++ 1.1.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    printf("\n\n");
+    printf("|----------------------------------------------------------------------------------|\n");
+    printf("|\t\t\t\t\t\t\t\t\t\t   |\n");
+
+    printf("|\t\t\t\t    JULIO'S BANK\t\t\t\t   |\n");
+    printf("------------------------------------------------------------------------------------\n");
+    printf("|\t\t||\t\t||\t\t||\t\t||\t   \t   |\n");
+    printf("|\t\t||\t\t||\t\t||\t\t||\t   \t   |\n");
+    printf("|\t\t||\t\t||\t\t||\t\t||\t   \t   |\n");
+    printf("|\t\t||\t\t||\t\t||\t\t||\t   \t   |\n");
+    printf("|\t\t||\t\t||\t\t||\t\t||\t   \t   |\n");
+    printf("|\t\t||\t\t||\t\t||\t\t||\t   \t   |\n");
+    printf("|\t\t||\t\t||\t\t||\t\t||\t   \t   |\n");
+    printf("|\t\t||\t\t||\t\t||\t\t||\t   \t   |\n");
+    printf("|_______________||______________||______________||______________||_________________|\n");
+
+
+    printf("|\t\t\b_/\t\t\b_/\t\t\\_\t\t\\_\t   \t   |\n");
+    printf("|\t\t\b\b\b_/\t\t\t\b\b\b_/\t\t\t\t\b\b\b\b\b\b\\_\t\t  \\_\t   \t   |\n");
+    printf("|\t\t\b\b\b\b\b_/\t\t\t\b\b\b\b\b_/\t\t\t\t\b\b\b\b\b\b  \\_\t\t    \\_\t   \t   |\n");
+    printf("|\t\t\b\b\b\b\b\b\b_/\t\t\t\b\b\b\b\b\b\b_/\t\t\t\t\b\b\b\b\b\b    \\_\t      \\_\t   |\n");
+    printf("|\t\t\b\b\b\b\b\b\b\b\b_/\t\t\t\b\b\b\b\b\b\b\b\b_/\t\t\t\t\b\b\b\b\b\b      \\_\t        \\_\t   |\n");
+    printf("|\t\t\b\b\b\b\b\b\b\b\b\b\b_/\t\t\t\b\b\b_/\t\t\t\t\b\b\b\b\b\b\t          \\_\t          \\_\t   |\n");
+    printf("|\t\t\b\b\b\b\b\b\b\b\b\b\b\b\b_/\t\t\t\b\b\b\b\b_/\t\t\t\t\b\b\b\b\b\b\t            \\_\t            \\_\t   |\n");
+    printf("|\t\t\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b_/\t\t\t\b\b\b\b\b\b\b_/\t\t\t\t\b\b\b\b\b\b\b\t              \\_\t      \\_\t\b\b\b\b\b|\n");
+    printf("|----------------------------------------------------------------------------------|\n");
+
+
+
+}
