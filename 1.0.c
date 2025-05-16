992afa7f11ec411443700a813895541d57b3a97e
Juliovillarruel <juliovillarruel2000@gmail.com>
2025-05-15 22:12:26 -0700

    Bank is done for now

--- 1.0.c
+++ 1.0.c
@@ -10,26 +10,19 @@ int main(void)
 
     printf("|\t\t\t\t    JULIO'S BANK\t\t\t\t   |\n");
     printf("------------------------------------------------------------------------------------\n");
-    printf("|\t\t|\t\t|\t\t|\t\t|\t   \t   |\n");
-    printf("|\t\t|\t\t|\t\t|\t\t|\t   \t   |\n");
-    printf("|\t\t|\t\t|\t\t|\t\t|\t   \t   |\n");
-    printf("|\t\t|\t\t|\t\t|\t\t|\t   \t   |\n");
-    printf("|\t\t|\t\t|\t\t|\t\t|\t   \t   |\n");
-    printf("|\t\t|\t\t|\t\t|\t\t|\t   \t   |\n");
-    printf("|\t\t|\t\t|\t\t|\t\t|\t   \t   |\n");
-    printf("|_______________|_______________|_______________|_______________|__________________|\n");
+    printf("|\t\t||\t\t||\t\t||\t\t||\t   \t   |\n");
+    printf("|\t\t||\t\t||\t\t||\t\t||\t   \t   |\n");
+    printf("|\t\t||\t\t||\t\t||\t\t||\t   \t   |\n");
+    printf("|\t\t||\t\t||\t\t||\t\t||\t   \t   |\n");
+    printf("|\t\t||\t\t||\t\t||\t\t||\t   \t   |\n");
+    printf("|\t\t||\t\t||\t\t||\t\t||\t   \t   |\n");
+    printf("|\t\t||\t\t||\t\t||\t\t||\t   \t   |\n");
+    printf("|\t\t||\t\t||\t\t||\t\t||\t   \t   |\n");
+    printf("|_______________||______________||______________||______________||_________________|\n");
 
 
     printf("|\t\t\b_/\t\t\b_/\t\t\\_\t\t\\_\t   \t   |\n");
-
     printf("|\t\t\b\b\b_/\t\t\t\b\b\b_/\t\t\t\t\b\b\b\b\b\b\\_\t\t  \\_\t   \t   |\n");
-
-
-
-
-
-
-
     printf("|\t\t\b\b\b\b\b_/\t\t\t\b\b\b\b\b_/\t\t\t\t\b\b\b\b\b\b  \\_\t\t    \\_\t   \t   |\n");
     printf("|\t\t\b\b\b\b\b\b\b_/\t\t\t\b\b\b\b\b\b\b_/\t\t\t\t\b\b\b\b\b\b    \\_\t      \\_\t   |\n");
     printf("|\t\t\b\b\b\b\b\b\b\b\b_/\t\t\t\b\b\b\b\b\b\b\b\b_/\t\t\t\t\b\b\b\b\b\b      \\_\t        \\_\t   |\n");
