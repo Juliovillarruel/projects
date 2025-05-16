fe3555b38da0be7c2b5a36be2e1881b35f3f0f51
Juliovillarruel <juliovillarruel2000@gmail.com>
2025-05-15 22:06:02 -0700

    Progress is being made

--- 1.0.c
+++ 1.0.c
@@ -4,6 +4,7 @@
 
 int main(void)
 {
+    printf("\n\n");
     printf("|----------------------------------------------------------------------------------|\n");
     printf("|\t\t\t\t\t\t\t\t\t\t   |\n");
 
@@ -16,7 +17,7 @@ int main(void)
     printf("|\t\t|\t\t|\t\t|\t\t|\t   \t   |\n");
     printf("|\t\t|\t\t|\t\t|\t\t|\t   \t   |\n");
     printf("|\t\t|\t\t|\t\t|\t\t|\t   \t   |\n");
-    printf("|\t\t|\t\t|\t\t|\t\t|\t   \t   |\n");
+    printf("|_______________|_______________|_______________|_______________|__________________|\n");
 
 
     printf("|\t\t\b_/\t\t\b_/\t\t\\_\t\t\\_\t   \t   |\n");
@@ -32,23 +33,10 @@ int main(void)
     printf("|\t\t\b\b\b\b\b_/\t\t\t\b\b\b\b\b_/\t\t\t\t\b\b\b\b\b\b  \\_\t\t    \\_\t   \t   |\n");
     printf("|\t\t\b\b\b\b\b\b\b_/\t\t\t\b\b\b\b\b\b\b_/\t\t\t\t\b\b\b\b\b\b    \\_\t      \\_\t   |\n");
     printf("|\t\t\b\b\b\b\b\b\b\b\b_/\t\t\t\b\b\b\b\b\b\b\b\b_/\t\t\t\t\b\b\b\b\b\b      \\_\t        \\_\t   |\n");
-    printf("|\t\t\b\b\b\b\b\b\b\b\b\b\b_/\t\t\t\b\b\b\b\b\b\b\b\b\b\b_/\t\t\t\t\b\b\b\b\b\b        \\_\t          \\_\t   |\n");
-    printf("|\t\t\b\b\b\b\b\b\b\b\b\b\b\b\b_/\t\t\t\b\b\b\b\b\b\b\b\b\b\b\b\b_/\t\t\t\t\b\b\b\b\b\b          \\_\t            \\_\t   |\n");
-    printf("|\t\t\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b_/\t\t/\t\t\\\t\t\\\t   \t   |\n");
-
-
-
-
-
-    printf("|\t\t\t\t\t\t\t\t\t\t   |\n");
-    printf("|\t\t\t\t\t\t\t\t\t\t   |\n");
-    printf("|\t\t\t\t\t\t\t\t\t\t   |\n");
-    printf("|\t\t\t\t\t\t\t\t\t\t   |\n");
-    printf("|\t\t\t\t\t\t\t\t\t\t   |\n");
-    printf("|\t\t\t\t\t\t\t\t\t\t   |\n");
-
-    printf("|----------------------------------------------------------------------------------|");
-
+    printf("|\t\t\b\b\b\b\b\b\b\b\b\b\b_/\t\t\t\b\b\b_/\t\t\t\t\b\b\b\b\b\b\t          \\_\t          \\_\t   |\n");
+    printf("|\t\t\b\b\b\b\b\b\b\b\b\b\b\b\b_/\t\t\t\b\b\b\b\b_/\t\t\t\t\b\b\b\b\b\b\t            \\_\t            \\_\t   |\n");
+    printf("|\t\t\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b_/\t\t\t\b\b\b\b\b\b\b_/\t\t\t\t\b\b\b\b\b\b\b\t              \\_\t      \\_\t\b\b\b\b\b|\n");
+    printf("|----------------------------------------------------------------------------------|\n");
 
 
 
