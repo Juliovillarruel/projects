d4bcd61fbe3a58042d7c3888d1d2f4012f977faf
Juliovillarruel <juliovillarruel2000@gmail.com>
2025-05-21 19:38:57 -0700

    Entirely Brand New Code

---
+++ 1.2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    
+    printf("Just one small step for coders. One giant leap for");
+    printf(" programmers!\n");
+    return 0;
+
+}
