cbfd6873bda4f790092bcbfe7f2342b8ad5d068e
Juliovillarruel <juliovillarruel2000@gmail.com>
2025-05-26 17:11:07 -0700

    New Update

---
+++ 1.5.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    
+    char answer; 
+    int quantity; 
+    float price;
+
+    char first_initial;
+    char middle_initial; 
+
+    // or
+
+    // char first_initial, middle_initial;
+
+    int variable = data; 
+
+}
